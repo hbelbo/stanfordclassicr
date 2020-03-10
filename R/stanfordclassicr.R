429164dd10a690fd9e3f8cc13e4635bbb0df23ec
Helmer Belbo <Helmer.Belbo@nibio.no>
2020-03-10 12:02:36 +0100

    Adding package help page. not sure it works.

---
+++ R/stanfordclassicr.R
@@ -0,0 +1,14 @@
+#' stanfordclassicr: A package for reading forest machine files following the
+#'   stanford classic standard (https://www.skogforsk.se/english/projects/stanford/)
+#'
+#' The package provide one function for each type of stanford standard file
+#'  and some supplementary functions for internal use.
+#'
+#'
+#' @section read_xyz_file
+#' The main functions read_ktr_file(), read_stm_file(), read_pri_file()
+#' open the file, extract data and organize in a list of data frames (tibbles).
+#'
+#' @docType package
+#' @name stanfordclassicr
+NULL
