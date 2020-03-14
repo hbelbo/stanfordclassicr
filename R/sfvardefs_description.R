d38e64e43d0267fe2490269d68c3c746ae0b89f0
Helmer Belbo <Helmer.Belbo@nibio.no>
2020-03-15 00:00:35 +0100

    Adding a list of all stanford classic variables

---
+++ R/sfvardefs_description.R
@@ -0,0 +1,22 @@
+#' StanForD classic variable definitions
+#'
+#' This dataset provides all variable names types and classes (integer / character)
+#' as defined in the StanForD classic Standard
+#'
+#' @docType data
+#'
+#' @usage data(sfvardefs)
+#'
+#' @format An object of class tibble.
+#'
+#' @keywords datasets
+#'
+#' @references
+#' (\href{https://www.skogforsk.se/english/projects/stanford/}{whattowrite})
+#'
+#' @source \href{https://www.skogforsk.se/english/projects/stanford/}{StanForD documentation}
+#'
+#' @examples
+#' data(sfvardefs)
+#' head(sfvardefs)
+"sfvardefs"
