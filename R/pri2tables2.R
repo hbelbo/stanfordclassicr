8e36a2b04a915f5d1be1f7d1c0facb9618ce72f0
Helmer Belbo <Helmer.Belbo@nibio.no>
2020-06-26 14:41:26 +0200

    Cosmetic edits

--- R/pri2tables2.R
+++ R/pri2tables2.R
@@ -74,7 +74,7 @@ read_pri_file2 <- function(filename){
   selector <- c( "v120t1", "v120t3")
   selected <- df1 %>% dplyr::select(., tidyselect::all_of(selector))
   dfx <- expand_str(selected)
-  dfx$tmp_species_nr = 1:nrow(species_group_definition)
+  dfx$tmp_species_nr = 1:nrow(dfx)
 
   species_group_definition <-
     dfx %>%  mutate(., species_group_name = v120t1,
@@ -136,11 +136,12 @@ read_pri_file2 <- function(filename){
   stems <- as_tibble(stems)
   #stems <- as.data.frame(stems)
   #names(stems) <- Code_Name_pairs_present$CodeN
-  stems$stem_key <- paste0(start_epoch,  stems$stemnumber)
-  stems$species_group_key <- paste0(start_epoch, species)
-  stems$object_key <- start_epoch
-
-  stems <- stems %>%
+  stems <-
+    stems %>%
+    dplyr::mutate(., stem_key = paste0(start_epoch, stemnumber),
+           species_group_key = paste0(start_epoch, species),
+           object_key = start_epoch
+           ) %>%
     dplyr::mutate(.,
                   latitude_category =
                     case_when(
@@ -159,10 +160,10 @@ read_pri_file2 <- function(filename){
   # Regarding Log_CodeCodes and names: IN SF2010 code 20 would be the same as Product Key, while code 1 would not have any direct equivalent as it is not a true unique key.
 
   Log_CodeCodes <- c(   1,               2,           20,                  201,       202,        203,          204,         205,         206,           207,              208,            300,        301,    302)
-  Log_CodeNames <- c("price_matrix_nr","species_nr","price_matrix_uid", "diatopob","diatopub","diamidob","diamidub","diabuttob","diabuttub","diahksmidob","diahksmidub","forcecut","lenght","Lenclass")
+  Log_CodeNames <- c("price_matrix_nr","species_nr","price_matrix_uid", "diatopob","diatopub","diamidob","diamidub","diabuttob","diabuttub","diahksmidob","diahksmidub","forcecut","length","Lenclass")
 
   Log_CodeCodes = c(Log_CodeCodes,  400,     1400,     401,     1401,           402,      1402,          403,        1403,          404,           1404,            405,           1405,          406,         1406,          420,         421,        422,         423,           424,              425,         426,             500,       501,         600,        2000)
-  Log_CodeNames = c(Log_CodeNames, "vol", "voldec", "vol_sob","vol_sob_dec","vol_sub","vol_sub_dec","vol_top_ob","vol_top_ob_dec","vol_top_ub","vol_top_ub_dec","vol_smi_ob","vol_smi_ob_dec","vol_smi_ub","vol_smi_ub_dec","vol_dl", "vol_dl_sob","vol_dl_sub","vol_dl_top_ob","vol_dl_top_ub","vol_dl_smi_ob","vol_dl_smi_ub","stem_no","log_no","numoflogs","userdefined")
+  Log_CodeNames = c(Log_CodeNames, "vol", "voldec", "vol_sob","vol_sob_dec","vol_sub","vol_sub_dec","vol_top_ob","vol_top_ob_dec","vol_top_ub","vol_top_ub_dec","vol_smi_ob","vol_smi_ob_dec","vol_smi_ub","vol_smi_ub_dec","vol_dl", "vol_dl_sob","vol_dl_sub","vol_dl_top_ob","vol_dl_top_ub","vol_dl_smi_ob","vol_dl_smi_ub","stem_nr","log_nr","numoflogs","userdefined")
 
   Log_Code_Name_pairs <- tibble(Code=Log_CodeCodes, CodeN= Log_CodeNames)
   Matches <- match(as.integer(unlist(stringr::str_split(df1$v256t1, " "))), Log_Code_Name_pairs$Code)
@@ -194,12 +195,15 @@ read_pri_file2 <- function(filename){
     mutate(.,
            product_key = case_when("price_matrix_uid" %in% names(LogData) ~  as.numeric(tmp_pk),
                                    TRUE ~ as.numeric(paste0(start_epoch, tmp_pk))),
-           stem_key = as.numeric(paste0(start_epoch, stem_no)),
-           log_key = log_no           )
+           stem_key = as.numeric(paste0(start_epoch, stem_nr)),
+           log_key = log_nr ) %>%
+    select(., stem_nr, log_nr, diatopub, length, m3price, m3sob, m3sub,
+           product_key, price_matrix_nr, stem_key, log_key,   )
 
 
 
-  Ret <- list(ReportHeader = ReportHeader,
+
+  Ret <- list(report_header = report_header,
               object_definition = object_definition,
               species_group_definition = species_group_definition,
               product_definition = product_definition,
